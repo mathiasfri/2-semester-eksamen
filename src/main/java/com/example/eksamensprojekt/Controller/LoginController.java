269f1ff496100fb0a2a6cbac1670d97017e0df72
mathiasfri <mathiasfridthjof@live.dk>
2023-04-24 13:45:38 +0200

    rename

--- src/main/java/com/example/eksamensprojekt/Controller/LoginController.java
+++ src/main/java/com/example/eksamensprojekt/Controller/LoginController.java
@@ -1,10 +1,10 @@
 package com.example.eksamensprojekt.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class LoginController {
     @GetMapping("")
     public String landingPage(){
         return "landingPage";
