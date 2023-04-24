0808ad626e9995956912f5868ebbdda7401731ae
mathiasfri <mathiasfridthjof@live.dk>
2023-04-24 13:51:20 +0200

    fixes

--- src/main/java/com/example/eksamensprojekt/Controller/LoginController.java
+++ src/main/java/com/example/eksamensprojekt/Controller/LoginController.java
@@ -2,7 +2,9 @@ package com.example.eksamensprojekt.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping()
 @Controller
 public class LoginController {
     @GetMapping("")
