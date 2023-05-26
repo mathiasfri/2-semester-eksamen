9f1b3d372b5c5ae17c8022411a8965d94dc4e79e
mathiasfri <mathiasfridthjof@live.dk>
2023-05-26 12:16:13 +0200

    delete function might work

--- src/main/java/com/example/eksamensprojekt/controller/ProjectController.java
+++ src/main/java/com/example/eksamensprojekt/controller/ProjectController.java
@@ -149,7 +149,7 @@ public class ProjectController {
         return "redirect:/projectCalculator/mainPage/" + projectUpdate.getUserId();
     }
 
-    @DeleteMapping("/deleteproject/{pid}")
+    @PostMapping("/deleteproject/{pid}")
     public String deleteProject(@PathVariable int pid, @ModelAttribute Project projectDelete) {
         projectRepository.deleteProject(pid);
         return "redirect:/projectCalculator/mainPage/" + projectDelete.getUserId();
